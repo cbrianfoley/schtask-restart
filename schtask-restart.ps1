704e17db5a978f9362419e7b1bbc207a2bd176bb
cbrianfoley <cbrianfoley@users.noreply.github.com>
2019-08-07 13:17:36 -0500

    Create schtask-restart.ps1

---
+++ schtask-restart.ps1
@@ -0,0 +1,8 @@
+﻿$RestartTime = Get-Date
+$RestartTime = $RestartTime.AddMinutes(10)
+$RestartTime = $RestartTime.ToString("hh:mmtt")
+$Action = New-ScheduledTaskAction -Execute 'shutdown.exe'-Argument '-r -t 60'
+$Trigger = New-ScheduledTaskTrigger -Once -At $RestartTime
+$Principal = New-ScheduledTaskPrincipal -RunLevel Highest -UserId SYSTEM
+$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
+Register-ScheduledTask -TaskName 'Restart' -InputObject $Task -User 'SYSTEM'
