b821fa8a550a47c442922e3664ee03768b27e1af
cbrianfoley <cbrianfoley@users.noreply.github.com>
2019-08-07 13:41:27 -0500

    Update schtask-restart.ps1

--- schtask-restart.ps1
+++ schtask-restart.ps1
@@ -1,9 +1,10 @@
-﻿Unregister-ScheduledTask -TaskName 'Restart'
+﻿Unregister-ScheduledTask -TaskName 'Restart' -Confirm:$false
 $RestartTime = Get-Date
 $RestartTime = $RestartTime.AddMinutes(10)
 $RestartTime = $RestartTime.ToString("hh:mmtt")
 $Action = New-ScheduledTaskAction -Execute 'shutdown.exe'-Argument '-r -t 60'
 $Trigger = New-ScheduledTaskTrigger -Once -At $RestartTime
 $Principal = New-ScheduledTaskPrincipal -RunLevel Highest -UserId SYSTEM
+$Settings = New-ScheduledTaskSettingsSet
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
 Register-ScheduledTask -TaskName 'Restart' -InputObject $Task -User 'SYSTEM'
